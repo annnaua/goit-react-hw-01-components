eb3df2ebd293e33dafdd4794ea79867d9c299834
Anna Riabova <annaua04@gmail.com>
2022-09-26 22:07:37 +0300

    add data

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    white: '#ffffff',
+    grey: '#ffffffb3',
+    black: '#1e1e1e',
+    dark: '#121212',
+  },
+  background: {
+    white: '#ffffff',
+    grey: '#ffffffb3',
+    greyDark: '#EEEEEE',
+    black: '#1e1e1e',
+    dark: '#121212',
+  },
+  status: {
+    red: '#D81B60',
+    green: '#00897B',
+  },
+};
